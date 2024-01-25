2d1f456e9d015788625710cd28682de33798f6b8
Sergii112 <srgkomlev@gmail.com>
2024-01-25 14:20:53 +0200

    add index.js file

---
+++ nodeBasics/index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('What is the difference between a programmer and a politician? '),
+    chalk.blue('The programmer is paid money for programs that work'));
