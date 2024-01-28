0dc2374bd0239e363f233c94135cef83b55f7606
Sergii112 <srgkomlev@gmail.com>
2024-01-28 15:49:57 +0200

    add

---
+++ intro_js_2/string.js
@@ -0,0 +1,24 @@
+//const greeting = "Hello JS!"
+
+//console.log(greeting[0])
+// console.log(greeting[1])
+// console.log(greeting[2])
+
+//Vuvede dovguny ryadka(kilkist sumvoliv)
+//console.log("length", greeting.length)
+
+//Vuvede ostaniy sumvol -2
+//console.log("last symbol", greeting[greeting.length - 2])
+
+//Konkotenaciya ryadkiv
+//const greeting = "Hello"
+// const name = "Peter"
+//
+// const greetingToPerson = greeting + " and " + name
+// console.log(greetingToPerson)
+
+//Vukorustovyem shablonnuy ryadok
+//const greeting = "Hello"
+// const name = "Peter"
+//const greetingToPerson = `${greeting} ${name}`
+// console.log(greetingToPerson)
---
+++ intro_js_2/variables.js
@@ -0,0 +1,11 @@
+//"const" ne zminna
+//const personAge = 22
+//console.log(personAge)
+
+// "let" mogna zminyuvatu
+
+//let name = "Serg"
+// console.log(name)
+// name = "Vova"
+// console.log(name)
+
