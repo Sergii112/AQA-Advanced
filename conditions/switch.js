984c609c5abb40ecb6b18e634778b156273ab1b8
Sergii112 <srgkomlev@gmail.com>
2024-02-03 22:38:04 +0200

    add

---
+++ conditions/switch.js
@@ -0,0 +1,14 @@
+
+// const userAge = 15
+// switch (userAge) {
+//     case 10:                      //userAge === 10 (stroge porivnyannya)
+//         console.log("10 years")
+//         break
+//     case 11:                      // userAge === 11
+//         console.log("11 years")
+//         break                     // yaksh prubratu "breake",to vukonaetsya pershuy pravulnuy case i
+//                                   // takog vsi nastypni, nezalegno pravulni vonu chu ni.
+//     default:
+//         console.log("Unhandled age")
+// }
+
