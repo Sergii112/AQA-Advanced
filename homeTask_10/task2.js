0e57ebdfb15f88c11e6591f967e3a6bc2e6d1a77
Sergii112 <srgkomlev@gmail.com>
2024-02-26 11:52:57 +0200

    add homeTask_10

---
+++ homeTask_10/task2.js
@@ -0,0 +1,35 @@
+function fetchToDo() {
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+      .then((response) => response.json())
+      .then(toDo => resolve(toDo))
+      .catch(error => reject(error))
+  })
+}
+
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+   fetch(`https://jsonplaceholder.typicode.com/users/1`)
+     .then((response) => response.json())
+     .then(user => resolve(user))
+     .catch(error => reject(error))
+  })
+}
+
+Promise.all([
+  fetchToDo(),
+  fetchUser()
+])
+  .then(result => {
+    console.log('All response result:', result)
+  })
+
+Promise.race([
+  fetchToDo(),
+  fetchUser()
+])
+  .then(result => {
+    console.log('Race response result:', result)
+  })
+
+
